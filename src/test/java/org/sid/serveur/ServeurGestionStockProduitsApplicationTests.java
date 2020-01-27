5fd35f8562ade0a0ed1e7b5f2e25d141f54ffda0
sido85 <sadekhelifa@gmail.com>
2020-01-27 17:50:16 +0100

    First commit

---
+++ src/test/java/org/sid/serveur/ServeurGestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.serveur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
